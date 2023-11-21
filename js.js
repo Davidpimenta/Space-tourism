1ff1999ca2757a2e52cdb9c34f73a9aa54952752
Mixtany <130236892+Davidpimenta@users.noreply.github.com>
2023-11-21 10:21:27 -0300

    Mobile V2

--- js.js
+++ js.js
@@ -6,6 +6,9 @@ function ativar(){
     const mobilemenu = document.getElementById('mobile-menu')
     mobilemenu.classList.toggle('mobile-menu-active')
     
+    const body = document.querySelector('body')
+    body.classList.toggle('body-active-menu')
+
     const liopt = document.querySelectorAll('.li-opt')
     console.log(liopt)
     liopt.forEach((e) =>{
